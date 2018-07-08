121ea374a829a3586c2229d1e3873125f648abb2
dhakegaurang <dhakegaurang.com>
2018-07-08 12:53:55 -0400

    Output written to respective output files and started with logger class implementation

--- studentCoursesBackup/src/studentCoursesBackup/driver/Driver.java
+++ studentCoursesBackup/src/studentCoursesBackup/driver/Driver.java
@@ -8,7 +8,7 @@ import studentCoursesBackup.util.TreeBuilder;
 public class Driver {
 
 	public static void main(String[] args) {
-
+		
 		String inputPath = "input/input_1.txt";
 		String deletePath = "delete/delete_1.txt";
 		String outputPath1 = "output1.txt";
@@ -18,24 +18,23 @@ public class Driver {
 		Results resultObj1 = new Results(outputPath1);
 		Results resultObj2 = new Results(outputPath2);
 		Results resultObj3 = new Results(outputPath3);
+		
 		TreeBuilder treeBuilderObj = new TreeBuilder(resultObj1,resultObj2,resultObj3);
+		
 		InputProcessor inputProcessorObj = new InputProcessor(treeBuilderObj);
+		
 		FileProcessor fileProcessorObj = new FileProcessor(inputPath,inputProcessorObj,"insert");
 		fileProcessorObj.readLine();
 		fileProcessorObj = new FileProcessor(deletePath,inputProcessorObj,"delete");
 		fileProcessorObj.readLine();
+		
 		treeBuilderObj.printNodes(resultObj1, treeBuilderObj.getOriginalNode());
 		treeBuilderObj.printNodes(resultObj2, treeBuilderObj.getObserverNode1());
 		treeBuilderObj.printNodes(resultObj3, treeBuilderObj.getObserverNode2());
-		//resultObj1.writeToFile(resultObj1.getResultStr());
-		System.out.println("----------Original------------");
-		System.out.println(resultObj1.getResultStr());
-		System.out.println("");
-		System.out.println("----------Backup1------------");
-		System.out.println(resultObj2.getResultStr());
-		System.out.println("");
-		System.out.println("----------Backup2------------");
-		System.out.println(resultObj3.getResultStr());
+		
+		resultObj1.writeToFile(resultObj1.getResultStr());
+		resultObj2.writeToFile(resultObj2.getResultStr());
+		resultObj3.writeToFile(resultObj3.getResultStr());
 	}
-
+		
 }
--- studentCoursesBackup/src/studentCoursesBackup/util/MyLogger.java
+++ studentCoursesBackup/src/studentCoursesBackup/util/MyLogger.java
@@ -1,5 +1,53 @@
 package studentCoursesBackup.util;
 
-public class MyLogger {
-	
+/**
+* Logger program helps in keeping track of program run
+*
+* @author  Gaurang Dhake
+* @version 1.0
+* @since   06/27/18 
+*/
+public class MyLogger{
+    // These are debugLevel values which represents states or classes
+    public static enum DebugLevel { FILE_PROCESSOR, NONE, NODE, INPUT_PROCESSOR,
+    								RESULTS, TREE_BUILDER
+                                   };
+    
+    private static DebugLevel debugLevel; // static current debugLevel for the program
+    
+    /**
+	   * This is method which assigns current debugLevel using command line argument
+	   * @param levelIn this parameter decides which debugLevel to set from cmd
+	   * @return DebugLevel returns debugLevel which was set here
+	   */
+    public static DebugLevel setDebugValue (int levelIn) {
+		switch (levelIn) {
+			case 1: debugLevel = DebugLevel.FILE_PROCESSOR; break;
+			case 2: debugLevel = DebugLevel.NODE; break;
+			case 3: debugLevel = DebugLevel.INPUT_PROCESSOR; break;
+			case 4: debugLevel = DebugLevel.RESULTS; break;
+			case 5: debugLevel = DebugLevel.TREE_BUILDER; break;
+			default: debugLevel = DebugLevel.NONE; break;
+		}
+		
+		return debugLevel;
+    }
+    
+    /**
+	   * This is method which writes message incoming from respective class
+	   * @param message this is the message to be printed
+	   * @param levelIn to check whether current level is the level being passed
+	   * @return None since return type is void
+	   */
+    public static void writeMessage (String message, DebugLevel levelIn ) {
+		if (levelIn == debugLevel)
+		    System.out.println(message);
+    }
+    /**
+	   * This is method returns string containing debugLevel
+	   * @return String it returns string for debugLevel
+	   */
+    public String toString() {
+    	return "The debug level has been set to the following " + debugLevel;
+    }
 }
